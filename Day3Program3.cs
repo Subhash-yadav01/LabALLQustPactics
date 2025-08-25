6cd74d1428bde025cd8044eba2e59ab63925b57a
Subhash Yadav <sy765272@gmail.com>
2025-08-25 10:56:23 -0700

    Code chang some line

--- Day3Program3.cs
+++ Day3Program3.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ Write a C# program to demonstrate the use of the boolean (bool) data type.
+create a class with the name "------" in that write the Main Method 
+In the Main Method:-
+Declare a bool variable (for example, isStudent) and assign it a value (true or false).
+Print the boolean value with a meaningful message.
+Use the .GetType() method to display the actual data type of the variable.
+
+ */
+
 namespace LabALLQustPactics
 {
     internal class Day3Program3
     {
+        static void Main(string[] args)
+        {
+            Boolean isStudent = true;
+            Console.WriteLine("Is the person a student? " + isStudent);
+
+            Console.WriteLine("The data type of isStudent is : " + isStudent.GetType());
+        }
     }
 }
