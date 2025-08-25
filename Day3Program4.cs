5db125d673ecbbd3daa4219b22017e2dc0ce5532
Subhash Yadav <sy765272@gmail.com>
2025-08-25 11:47:54 -0700

    spaces

--- Day3Program4.cs
+++ Day3Program4.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 
 /*
- Write a C# program to demonstrate the use of GUIDs (Globally Unique Identifiers).
-   create a class with the name "Gunid" in that write the Main Method 
-   In the Main Method:-
-   Declare a Guid  two variables (id1 and id2) using Guid.NewGuid().
-   Print both GUID values 
-   Use the .GetType() method to display the actual data type of the variable.
+     Write a C# program to demonstrate the use of GUIDs (Globally Unique Identifiers).
+     create a class with the name "-----" in that write the Main Method 
+     In the Main Method:-
+     Declare a Guid  two variables (id1 and id2) using Guid.NewGuid().
+     Print both GUID values 
+     Use the .GetType() method to display the actual data type of the variable.
 
  */
 
@@ -19,5 +19,16 @@ namespace LabALLQustPactics
 {
     internal class Day3Program4
     {
+        static void Main(string[] args)
+        {
+            Guid id1 = Guid.NewGuid();
+            Guid id2 = Guid.NewGuid();
+
+            Console.WriteLine("The first Guid: " + id1);
+            Console.WriteLine("The second Guid: " + id2);
+
+            Console.WriteLine("The data type of id1 is: " + id1.GetType());
+            Console.WriteLine("The data type of id2 is: " + id2.GetType());
+        }
     }
 }
